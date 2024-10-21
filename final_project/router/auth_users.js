e93899eaedf45a18c7bbcd6f1b92988dc892ffc6
Aonai-Lin <66240464+Aonai-Lin@users.noreply.github.com>
2024-10-21 21:26:51 +0800

    accomplished homework

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,25 +6,85 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  // extract all the user with that name
+  let userwithsamename = users.filter(user=>user.username===username);
+  if(userwithsamename.length>0){
+    return false;   // name already exist, so not valid
+  }else{
+    return true;
+  }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  // filter users with the same name and password, if exist, it is authenticated
+  let validusers = users.filter(user=>user.username===username && user.password===password);
+  if(validusers.length>0){
+    return true;  // if there is such a user in the users, it is authenticated
+  }else{
+    return false;
+  }
+
+
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // extract username and password in req
+  const username = req.body.username;
+  const password = req.body.password;
+  // if either is empty, raise error
+  if(!username || !password){
+    res.status(404).send("Empty username or password, please check");
+  }
+  // to authenticate user, if true, generate a token for the req
+  if(authenticatedUser(username, password)){
+    let accessToken = jwt.sign({date: password}, 'access', {expiresIn: 60*60});
+    req.session.authorization = {accessToken, username};
+    return res.status(200).send(`User ${username} login successfully!`);
+  }else{
+    return res.status(404).send("Not authenticated user, please register first!");
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // get the username from session, the isbn from request param and the context from request query
+  const username = req.session.authorization['username'];
+  const isbn = req.params.isbn;
+  const review  = req.query.review;
+  // find the target book and update/add the review
+  books[isbn]["reviews"][username] = review;
+  // console.log(books);
+  return res.status(200).send(`User ${username} 's review updated successfully!`);
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res)=>{
+  // get thr username from session, the isbn from request param
+  const username = req.session.authorization['username'];
+  const isbn = req.params.isbn;
+  // console.log("username: "+username+", isbn: "+isbn)
+
+  // find the target book and delete the user's review
+  const targetbook = books[isbn];
+  delete targetbook["reviews"][username];
+  return res.status(200).send("The target review was deleted");
+
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+
+
+/*
+确认对象是否有某属性：
+  1、'propertyName' in object
+  2、object.hasOwnProperty('propertyName')
+  3、if (object.propertyName !== undefined) {
+      // 属性存在，但这种方式会无法区分属性不存在与属性值为 undefined 的情况。
+    }
+
+新增属性/修改属性都用“.”或者"[]"
+删除属性：delete object.property或delete object["property"]
+*/
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,169 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
+
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // extract username and password in req body
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(!username || !password){
+    return res.status(404).send("Invalid username or password!");
+  }
+
+  if(isValid(username)){
+    users.push({"username":username, "password":password});
+    return res.status(200).send( `User ${username} successfully registered. Now you can login`);
+  }else{
+    return res.status(404).send("Username already existed!")
+  }
 });
 
 // Get the book list available in the shop
+// sync：
+// public_users.get('/',function (req, res) {
+//   res.send(JSON.stringify(books, null, 4));
+// });
+// async：
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // assume that we need to fetch the books from 'https://example.com/api/books'
+  axios.get('https://example.com/api/books')
+  .then(response=>{
+    res.send(JSON.stringify(response.data, null, 4)); 
+  }).catch(err=>{
+    res.status(500).send('Error fetching books');
+  });
+
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// sync
+// public_users.get('/isbn/:isbn',function (req, res) {
+//   // extract the isbn param
+//   const isbn = req.params.isbn;
+//   const targetBook = books[isbn];
+//   return res.send(JSON.stringify(targetBook, null, 4));
+
+//  });
+ // async
+ public_users.get('/isbn/:isbn',function (req, res) {
+  // assume that we need to fetch the books from 'https://example.com/api/books'
+  axios.get('https://example.com/api/books')
+  .then(response=>{
+    let books = response.data;
+    const isbn = req.params.isbn;
+    const targetBook = books[isbn];
+    return res.send(JSON.stringify(targetBook, null, 4));
+  }).catch(err=>{
+    res.status(500).send('Error fetching books');
+  })
  });
+
+
+
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// sync
+// public_users.get('/author/:author',function (req, res) {
+//   // extract the author params and create a booklist for return
+//   const author = req.params.author;
+//   let booklist = [];
+//   // iterate the books object and collect books with the matched author
+//   for (let i in books){
+//     if(books[i].author===author){
+//       booklist.push(books[i]);
+//     }
+//   }
+//   if(booklist.length>0){
+//     return res.send(JSON.stringify(booklist, null, 4));
+//   }else{
+//     return res.status(403).json({message:"not such a author"});
+//   }
+// });
+// async
+// assume that we need to fetch the books from 'https://example.com/api/books'
+public_users.get('/author/:author', async function (req, res) {
+  try{
+    const response = await axios.get('https://example.com/api/books');
+    const books = response.data;
+    // process the data
+    const author = req.params.author;
+    let booklist = [];
+    // iterate the books object and collect books with the matched author
+    for (let i in books){
+      if(books[i].author===author){
+        booklist.push(books[i]);
+      }
+    }
+    if(booklist.length>0){
+      return res.send(JSON.stringify(booklist, null, 4));
+    }else{
+      return res.status(403).json({message:"not such a author"});
+    }
+  }catch (error){
+      res.status(500).send('Error fetching books');
+  }
 });
 
+
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// sync
+// public_users.get('/title/:title',function (req, res) {
+//   // extract the title params and create a booklist for return
+//   const title = req.params.title;
+//   let booklist = [];
+//   // iteater books and collect books with the matched title
+//   for (let i in books){
+//     if(books[i].title===title){
+//       booklist.push(books[i]);
+//     }
+//   }
+//   if(booklist.length>0){
+//     return res.send(JSON.stringify(booklist, null, 4));
+//   }else{
+//     return res.status(403).json({message: "not such a title"});
+//   }
+// });
+// async
+// assume that we need to fetch the books from 'https://example.com/api/books'
+public_users.get('/title/:title', async function (req, res) {
+  try{
+    // fetch the books
+    const response = await axios.get('https://example.com/api/books');
+    const books = response.data;
+    // process data
+    // extract the title params and create a booklist for return
+    const title = req.params.title;
+    let booklist = [];
+    // iteater books and collect books with the matched title
+    for (let i in books){
+      if(books[i].title===title){
+        booklist.push(books[i]);
+      }
+    }
+    if(booklist.length>0){
+      return res.send(JSON.stringify(booklist, null, 4));
+    }else{
+      return res.status(403).json({message: "not such a title"});
+    }
+  }catch(error){
+    res.status(500).send('Error fetching books');
+  }
 });
+
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // extract the isbn from params
+  const isbn = req.params.isbn;
+  // get the target book base on isbn and get the review
+  const review = books[isbn].review;
+  res.send(review);
 });
+
 
 module.exports.general = public_users;
